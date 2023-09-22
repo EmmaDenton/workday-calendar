912366248a77b980b5508686a649107115b8c363
Emma Denton <emmad@192-168-1-107.tpgi.com.au>
2023-09-22 23:01:06 +1000

    Fixed text area function so that local storage data shows on page. Added current date to header. Added if/else conditions to handle colouring past, present and future hours.

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,36 @@
 $(document).ready(function() {
+  function displaytime() {
+    $("#currentDay").text(dayjs().format('DD/MM/YYYY'));
+  }
 
-  const saveButton = document.querySelector('button[aria-label="save"]');
-  saveButton.addEventListener('click', function() {
+  displaytime();
+
+  const saveButton = $('button[aria-label="save"]');
+  saveButton.on('click', function() {
     var timeBlockId = $(this).parent().attr("id");
     var userInput = $(this).siblings("textarea").val();
     localStorage.setItem(timeBlockId, userInput);
   });
 
-  $(".time-block").each(function() {
-    var timeBlockId = $(this).attr("id");
+  $(".time-block textarea").each(function() {
+    var timeBlockId = $(this).parent().attr("id");
     var userInput = localStorage.getItem(timeBlockId);
-    $(this).find("textarea").val(userInput);
+    $(this).val(userInput);
   });
 
   $(".time-block").each(function() {
     var timeBlockId = $(this).attr("id");
     var currentHour = parseInt(dayjs().format('HH'));
-    console.log(currentHour)
-    })
+    var timeBlockHour = parseInt(timeBlockId.split("-")[1]);
+
+    if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+    } else if (timeBlockHour < currentHour) {
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else {
+      $(this).removeClass("past present");
+      $(this).addClass("future");
+    }
   });
+});
--- README.md
+++ README.md
@@ -1,20 +1,18 @@
-## Your Task
+## Work Day Calendar
+This project is a 9am&ndash;5pm day planner. 
 
-Create a simple calendar application that allows a user to save events for each hour of a typical working day (9am&ndash;5pm) by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+The hour will change colour to indicate if it is in the present, past or future. Hours in the present are red, hours in the past are grey and hours in the future are green. 
 
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+The user can input text into the center column for each hour. They can click the blue save button to save their entry to the their local storage.
 
 ## User Story
 
-```md
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
-```
 
 ## Acceptance Criteria
 
-```md
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
@@ -28,4 +26,3 @@ WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
-```
